f96cd4fbd5b9d440299738b0a74274b3a958f5f0
kazukitakao55@gmail.com <kazukitakao55@gmail.com>
2022-02-12 18:53:34 +0900

    アイテムの取得判定部分の実装

--- submarine-online/game_server/game.js
+++ submarine-online/game_server/game.js
@@ -14,7 +14,11 @@ const gameObj = {
   fieldHeight: 1000,
   // アイテム出現数
   itemTotal: 15,
-  airTotal: 10
+  airTotal: 10,
+  itemRadius: 4,
+  airRadius: 6,
+  addAirTime: 30,
+  submarineImageWidth: 42
 };
 
 function init() {
@@ -29,6 +33,7 @@ init(); // 初期化（初期化はサーバー起動時に行う
 
 const gameTicker = setInterval(() => {
   movePlayers(gameObj.playersMap); // 潜水艦の移動
+  checkGetItem(gameObj.playersMap, gameObj.itemsMap, gameObj.airMap); // アイテムの取得チェック
 }, 33);
 
 /**
@@ -64,6 +69,79 @@ function movePlayers(playersMap) { // 潜水艦の移動
     if (player.x < 0) player.x += gameObj.fieldWidth;
     if (player.y < 0) player.y += gameObj.fieldHeight;
     if (player.y > gameObj.fieldHeight) player.y -= gameObj.fieldHeight;
+
+    player.aliveTime.clock += 1;
+    if (player.aliveTime.clock === 30) {
+      player.aliveTime.clock = 0;
+      player.aliveTime.seconds += 1;
+      decreaseAir(player);
+      player.score += 1;
+    }
+
+  }
+}
+
+/**
+ * 酸素をへらす処理
+ * @param {*} playerObj 
+ */
+function decreaseAir(playerObj) {
+  playerObj.airTime -= 1;
+  if (playerObj.airTime === 0) {
+    playerObj.isAlive = false;
+  }
+}
+
+/**
+ * アイテムとの当たり判定を行う関数
+ * @param {*} playersMap 
+ * @param {*} itemsMap 
+ * @param {*} airMap 
+ */
+function checkGetItem(playersMap, itemsMap, airMap) {
+  for (let [hashKey, playerObj] of playersMap) {
+    if (playerObj.isAlive === false) continue;
+
+    // アイテムのミサイル（赤丸）
+    for (let [itemKey, itemObj] of itemsMap) {
+
+      const distanceObj = calculationBetweenTwoPoints(
+        playerObj.x, playerObj.y, itemObj.x, itemObj.y, gameObj.fieldWidth, gameObj.fieldHeight
+      );
+
+      // X軸とY軸方向に潜水艦の半分とアイテムの半分を足した幅以内であればアイテムと接触したと判定する
+      if (
+        distanceObj.distanceX <= (gameObj.submarineImageWidth / 2 + gameObj.itemRadius) &&
+        distanceObj.distanceY <= (gameObj.submarineImageWidth / 2 + gameObj.itemRadius)
+      ) { // got item!
+
+        gameObj.itemsMap.delete(itemKey);
+        playerObj.missilesMany = playerObj.missilesMany > 5 ? 6 : playerObj.missilesMany + 1;
+        addItem();
+      }
+    }
+
+    // アイテムの空気（青丸）
+    for (let [airKey, airObj] of airMap) {
+
+      const distanceObj = calculationBetweenTwoPoints(
+        playerObj.x, playerObj.y, airObj.x, airObj.y, gameObj.fieldWidth, gameObj.fieldHeight
+      );
+
+      if (
+        distanceObj.distanceX <= (gameObj.submarineImageWidth / 2 + gameObj.airRadius) &&
+        distanceObj.distanceY <= (gameObj.submarineImageWidth / 2 + gameObj.airRadius)
+      ) { // got air!
+
+        gameObj.airMap.delete(airKey);
+        if (playerObj.airTime + gameObj.addAirTime > 99) {
+          playerObj.airTime = 99;
+        } else {
+          playerObj.airTime += gameObj.addAirTime;
+        }
+        addAir();
+      }
+    }
   }
 }
 
@@ -87,6 +165,9 @@ function newConnection(socketId, displayName, thumbUrl) {
     thumbUrl: thumbUrl,
     isAlive: true,
     direction: 'right',
+    missilesMany: 0,
+    airTime: 99,
+    aliveTime: { 'clock': 0, 'seconds': 0 },
     score: 0
   };
   gameObj.playersMap.set(socketId, playerObj);
@@ -118,6 +199,8 @@ function getMapData() {
     playerDataForSend.push(plyer.score);
     playerDataForSend.push(plyer.isAlive);
     playerDataForSend.push(plyer.direction);
+    playerDataForSend.push(plyer.missilesMany);
+    playerDataForSend.push(plyer.airTime);
 
     playersArray.push(playerDataForSend);
   }
@@ -183,6 +266,65 @@ function addAir() {
   };
   gameObj.airMap.set(airKey, airObj);
 }
+
+/**
+ * 衝突判定用の2点間の距離を求める関数
+ * @param {*} pX 
+ * @param {*} pY 
+ * @param {*} oX 
+ * @param {*} oY 
+ * @param {*} gameWidth 
+ * @param {*} gameHeight 
+ * @returns 
+ */
+function calculationBetweenTwoPoints(pX, pY, oX, oY, gameWidth, gameHeight) {
+  let distanceX = 99999999;
+  let distanceY = 99999999;
+
+  if (pX <= oX) {
+    // 右から
+    distanceX = oX - pX;
+    // 左から
+    let tmpDistance = pX + gameWidth - oX;
+    if (distanceX > tmpDistance) {
+      distanceX = tmpDistance;
+    }
+
+  } else {
+    // 右から
+    distanceX = pX - oX;
+    // 左から
+    let tmpDistance = oX + gameWidth - pX;
+    if (distanceX > tmpDistance) {
+      distanceX = tmpDistance;
+    }
+  }
+
+  if (pY <= oY) {
+    // 下から
+    distanceY = oY - pY;
+    // 上から
+    let tmpDistance = pY + gameHeight - oY;
+    if (distanceY > tmpDistance) {
+      distanceY = tmpDistance;
+    }
+
+  } else {
+    // 上から
+    distanceY = pY - oY;
+    // 下から
+    let tmpDistance = oY + gameHeight - pY;
+    if (distanceY > tmpDistance) {
+      distanceY = tmpDistance;
+    }
+  }
+
+  return {
+    distanceX,
+    distanceY
+  };
+}
+
 
 module.exports = {
   newConnection,
